022f43552a550f0fde701ada7d9f9d962498b045
Benedict Florance <benedictflorance.a@gmail.com>
2020-10-08 02:46:37 +0530

    Fix path

--- data/voc0712.py
+++ data/voc0712.py
@@ -21,7 +21,7 @@ VOC_CLASSES = (  # always index 0
     'product')
 
 # note: if you used our download scripts, this should be right
-VOC_ROOT = "../../VOCdevkit"
+VOC_ROOT = "./../VOCdevkit"
 
 
 class VOCAnnotationTransform(object):
