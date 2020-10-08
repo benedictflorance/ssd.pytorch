ba8a89a18f63b0c86a3933cab0f802ea9cc0a5d3
Benedict Florance <benedictflorance.a@gmail.com>
2020-10-08 09:40:11 +0530

    Debug dataloader

--- train.py
+++ train.py
@@ -74,7 +74,8 @@ def train():
         dataset = VOCDetection(root=args.dataset_root,
                                transform=SSDAugmentation(cfg['min_dim'],
                                                          MEANS))
-
+    print("Minimum number of dimensions" cfg['min_dim'])
+    print("Number of classes", cfg['num_classes'])
     if args.visdom:
         import visdom
         viz = visdom.Visdom()
