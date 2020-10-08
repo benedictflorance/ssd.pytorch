d5f8477d7e53714fdd3e38a7f79d956e6a8fc440
Benedict Florance <benedictflorance.a@gmail.com>
2020-10-08 12:24:03 +0530

    Change log interval

--- train.py
+++ train.py
@@ -184,7 +184,7 @@ def train():
             update_vis_plot(iteration, loss_l.item(), loss_c.item(),
                             iter_plot, epoch_plot, 'append')
 
-        if iteration != 0 and iteration % 5000 == 0:
+        if iteration != 0 and iteration % 50 == 0:
             print('Saving state, iter:', iteration)
             torch.save(ssd_net.state_dict(), 'weights/ssd300_COCO_' +
                        repr(iteration) + '.pth')
