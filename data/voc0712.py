11c6432161f3aa2a51811dc877d77abe46f25dbd
Benedict Florance <benedictflorance.a@gmail.com>
2020-10-08 11:19:45 +0530

    Debug getitem

--- data/voc0712.py
+++ data/voc0712.py
@@ -6,6 +6,7 @@ https://github.com/fmassa/vision/blob/voc_dataset/torchvision/datasets/voc.py
 Updated by: Ellis Brown, Max deGroot
 """
 from .config import HOME
+import os
 import os.path as osp
 import sys
 import torch
